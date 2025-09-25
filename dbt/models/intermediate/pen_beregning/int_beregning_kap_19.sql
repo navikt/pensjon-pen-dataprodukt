10c4c106b95d980adb0d807adb7cc67292a42c20
Eystein Waade <eystein.waade@nav.no>
2025-09-25 12:28:38 +0200

    🚧 kap19 Co-authored-by: Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>

---
+++ dbt/models/intermediate/pen_beregning/int_beregning_kap_19.sql
@@ -0,0 +1,70 @@
+with
+
+ref_int_lopende_vedtak_alder as (
+    select
+        vedtak_id,
+        sak_id,
+        kravhode_id,
+        k_sak_t,
+        dato_lopende_fom,
+        dato_lopende_tom,
+        k_regelverk_t
+    from {{ ref('int_lopende_vedtak_alder') }}
+),
+
+ref_beregning as (
+    -- kapittel 19
+    select
+        vedtak_id,
+        beregning_id,
+        total_vinner,
+        dato_virk_fom,
+        dato_virk_tom,
+        k_minstepensj_t,
+        red_pga_inst_opph,
+        brutto,
+        netto,
+        yug,
+        tt_anv
+    from {{ ref('stg_t_beregning') }}
+    -- from pen.t_beregning
+),
+
+join_beregning as (
+    select
+        ref_int_lopende_vedtak_alder.*,
+        ref_beregning.beregning_id,
+        ref_beregning.k_minstepensj_t,
+        ref_beregning.red_pga_inst_opph,
+        ref_beregning.brutto,
+        ref_beregning.netto,
+        ref_beregning.yug,
+        ref_beregning.tt_anv
+    from ref_int_lopende_vedtak_alder
+    inner join ref_beregning
+        on
+            ref_int_lopende_vedtak_alder.vedtak_id = ref_beregning.vedtak_id
+            and ref_beregning.total_vinner = '1'
+            and ref_beregning.dato_virk_fom <= {{ periode_sluttdato(var("periode")) }}
+            and (ref_beregning.dato_virk_tom is null or ref_beregning.dato_virk_tom >= trunc({{ periode_sluttdato(var("periode")) }}))
+)
+
+select
+    vedtak_id,
+    sak_id,
+    kravhode_id,
+    k_sak_t,
+    k_regelverk_t,
+    dato_lopende_fom,
+    dato_lopende_tom,
+
+    brutto,
+    netto,
+
+    beregning_id,
+
+    k_minstepensj_t,
+    red_pga_inst_opph,
+    yug,
+    tt_anv
+from join_beregning
