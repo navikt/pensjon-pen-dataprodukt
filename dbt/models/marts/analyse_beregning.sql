30abe807f6cf878da7737660a59e6087645d84a1
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-10-07 12:50:41 +0200

    🚑 Spesifiserer eksplisitt kolonner i inkrementell analysemodeller for beregning og beløp, slik at vi kan endre underliggende modeller. Fjerner også duplikatinfo

--- dbt/models/marts/analyse_beregning.sql
+++ dbt/models/marts/analyse_beregning.sql
@@ -5,10 +5,34 @@
 }}
 
 select
-    b.*,
+    sak_id,
+    vedtak_id,
+    k_regelverk_t,
+    brutto,
+    netto,
+
+    uttaksgrad,
+    tt_anv_g_opptj,
+    tt_anv_n_opptj,
+
+    -- flagg
+    institusjon_opphold,
+    anvendt_yrkesskade_flagg,
+    gjenlevrett_anv,
+    minstepensjon,
+
+    pen_under_utbet_id,
+    beregning_id,
+
+    -- fjern etter analyse, hører hjemme i vedtak
+    cast(null as number) as kravhode_id,  -- fjern etter analyse, hører hjemme i vedtak
+    cast(null as varchar2(5)) as k_sak_t,  -- fjern etter analyse, hører hjemme i vedtak
+    cast(null as date) as dato_lopende_fom,  -- fjern etter analyse, hører hjemme i vedtak
+    cast(null as date) as dato_lopende_tom,  -- fjern etter analyse, hører hjemme i vedtak
+
     {{ var("periode") }} as periode,
     sysdate as kjoretidspunkt
-from {{ ref('int_beregning') }} b
+from {{ ref('int_beregning') }}
 where
     1 = 1
 {% if is_incremental() %}
--- dbt/models/marts/analyse_beregning_belop.sql
+++ dbt/models/marts/analyse_beregning_belop.sql
@@ -5,10 +5,43 @@
 }}
 
 select
-    bb.*,
+    vedtak_id,
+    sak_id,
+    beregning_id,
+    pen_under_utbet_id,
+    brutto,
+    netto,
+
+    sum_fradrag,
+    k_minstepen_niva as max_k_minstepen_niva,
+    k_minstepen_niva as min_k_minstepen_niva, -- denne kan fjernes fra denne inkrementell modellen, er alltid lik max
+
+    gp_netto,
+    tp_netto,
+    pt_netto,
+    st_netto,
+    ip_netto,
+    et_netto,
+    vt_netto,
+    gap_netto,
+    gat_netto,
+    gjt_netto,
+    afp_t_netto,
+    saerkull_netto,
+    barn_felles_netto,
+    skjermt_netto,
+    gjt_k19_netto,
+    ufor_sum_ut_ord_netto,
+    afp_livsvarig_netto,
+    mpn_indiv_netto,
+    mpn_sstot_netto,
+
+    ap_kap19_med_gjr_bel,
+    ap_kap19_uten_gjr_bel,
+    gp_avkorting_flagg,
     {{ var("periode") }} as periode,
     sysdate as kjoretidspunkt
-from {{ ref('int_beregning_belop') }} bb
+from {{ ref('int_beregning_belop') }}
 where
     1 = 1
 {% if is_incremental() %}
