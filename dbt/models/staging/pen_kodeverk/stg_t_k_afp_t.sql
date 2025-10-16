5ff3af1558a8765dc23819fe8e7dfb7067e1fe4c
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-10-16 11:04:14 +0200

    🔥 sletter ubrukte staging-modeller

--- dbt/models/staging/pen_kodeverk/stg_t_k_afp_t.sql
+++
@@ -1,6 +0,0 @@
--- stg_t_k_afp_t
-
-select
-    k_afp_t,
-    dekode
-from {{ source('pen', 't_k_afp_t') }}
--- dbt/models/staging/pen_kodeverk/stg_t_k_regelverk_t.sql
+++
@@ -1,6 +0,0 @@
--- stg_t_k_regelverk_t
-
-select
-    k_regelverk_t,
-    dekode
-from {{ source('pen', 't_k_regelverk_t') }}
