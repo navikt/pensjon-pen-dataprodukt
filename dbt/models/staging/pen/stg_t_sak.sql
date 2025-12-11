f58b81b09cc747b00b8463e55b73e333af1d8afe
Eystein Waade <eystein.waade@nav.no>
2025-12-11 11:13:34 +0100

    🐛 legg til stagingmodell for t_sak Co-authored-by: Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>

---
+++ dbt/models/staging/pen/stg_t_sak.sql
@@ -0,0 +1,13 @@
+-- stg_t_sak
+
+select
+    sak_id,
+    k_sak_t,
+    dato_opprettet,
+    opprettet_av,
+    dato_endret,
+    endret_av,
+    versjon,
+    k_sak_s,
+    k_utlandstilknytning
+from {{ source('pen', 't_sak') }}
