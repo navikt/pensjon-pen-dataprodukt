22a2af1d2c5d0b6b56c09614130a40bd9dd5f835
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2026-03-11 16:27:47 +0100

    📈 Sjekk på om bruker har avkortet UT i fbm EPS sitt BT

--- dbt/models/marts/analyse/analyse_lovendringer_eps_og_bt.sql
+++ dbt/models/marts/analyse/analyse_lovendringer_eps_og_bt.sql
@@ -6,29 +6,38 @@ finne_eps_til_lovendring as (
     select
         v.sak_id,
         v.vedtak_id,
-        v.oifu_endret_flagg,
         pg.person_id,
-        v.minsteytelse_ektefelle_flagg,
+        v.oifu_endret_flagg as ifu_opp,
+        v.minsteytelse_ektefelle_flagg as min_ektef,
+        v.kompgrad_over_70_flagg as kompgrad_70,
         case when (
             v.er_minimums_ifu = '1'
             and v.oifu in (429527, 429528, 429529)
             and v.grad != 100
             and v.k_minsteytelseniva in ('ORDINER_EKTEFELLE', 'ORDINER_EKTEF_KONV')
-            --and k_minimum_ifu_t = 'MINIMUM_IFU_GIFT'
             and v.ny_avrundet_grad - v.grad != 0
         ) or (
             v.oifu > 429529
             and v.oifu < 455560
             and v.grad != 100
             and v.ny_avrundet_grad - v.grad != 0
-        ) then '5%' else '-' end as gradsendring,
-        v.kompgrad_over_70_flagg,
+        ) then 'Bruker får 5% økt grad' end as gradsendring,
+        case when yk.netto < yk.brutto then 'Brukers UT er avkortet' end as ut_avkoret,
         pd.k_grnl_rolle_t,
         pg.kravhode_id as eps_kravhode_id
     from pen_dataprodukt.analyse_ifu_ieu_grad v
     left join pen.t_sak s on v.sak_id = s.sak_id
     left join pen.t_person_grunnlag pg on s.person_id = pg.person_id
     left join pen.t_person_det pd on pg.person_grunnlag_id = pd.person_grunnlag_id
+    left join pen.t_beregning_res ber
+        on
+            v.vedtak_id = ber.vedtak_id
+            and ber.dato_virk_tom is null
+    left join pen.t_ytelse_komp yk
+        on
+            ber.pen_under_utbet_id = yk.pen_under_utbet_id
+            and yk.k_ytelse_komp_t = 'UT_ORDINER'
+            and yk.bruk = '1'
     where
         pd.k_grnl_rolle_t in ('EKTEF', 'PARTNER', 'SAMBO')
         and (
@@ -69,26 +78,38 @@ eps_vedtak_lopende as (
 
 select
     count(*) as antall,
-    oifu_endret_flagg as okt_ifu,
-    minsteytelse_ektefelle_flagg as ny_minsteytelse_ekft,
-    kompgrad_over_70_flagg as kompgrad_over_70,
+    ifu_opp,
+    min_ektef,
+    kompgrad_70,
+    coalesce(coalesce(gradsendring, ut_avkoret), ' ') as avkort_info,
     case
         when tfb_netto > 0 and tfb_netto = tfb_brutto then 'TFB uavkortet'
         when tfb_netto > 0 and tfb_netto < tfb_brutto then 'TFB delvis avkortet'
         when tfb_netto = 0 then 'TFB fullt avkortet'
         when tfb_netto is null then 'ingen TFB'
-        else 'obs!'
-    end as bt_fb
+    end as bt_fb,
+    case
+        when tfb_netto > 0 and tfb_netto = tfb_brutto then '1'
+        when tfb_netto > 0 and tfb_netto < tfb_brutto then '2'
+        when tfb_netto = 0 then '3'
+        when tfb_netto is null then '4'
+    end as sortering
 from eps_vedtak_lopende
 group by
-    oifu_endret_flagg,
-    minsteytelse_ektefelle_flagg,
-    kompgrad_over_70_flagg,
+    ifu_opp,
+    min_ektef,
+    kompgrad_70,
+    coalesce(gradsendring, ut_avkoret),
     case
         when tfb_netto > 0 and tfb_netto = tfb_brutto then 'TFB uavkortet'
         when tfb_netto > 0 and tfb_netto < tfb_brutto then 'TFB delvis avkortet'
         when tfb_netto = 0 then 'TFB fullt avkortet'
         when tfb_netto is null then 'ingen TFB'
-        else 'obs!'
+    end,
+    case
+        when tfb_netto > 0 and tfb_netto = tfb_brutto then '1'
+        when tfb_netto > 0 and tfb_netto < tfb_brutto then '2'
+        when tfb_netto = 0 then '3'
+        when tfb_netto is null then '4'
     end
-order by bt_fb desc, antall desc
+order by sortering asc, antall desc
