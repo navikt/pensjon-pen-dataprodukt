64d5abd16a06f6a4a41088cb465ad686b406607a
Eystein Waade <eystein.waade@nav.no>
2026-09-08 11:13:54 +0200

    🐛 Legg til kjoretidspunkt i styringsinfo sak uføre
    Co-authored-by: Lise Nguyen <lise.nguyen@nav.no>

--- models/marts/styringsinfo_sak_uforetrygd.sql
+++ models/marts/styringsinfo_sak_uforetrygd.sql
@@ -35,5 +35,6 @@ select
     cast(null as date) as avsluttet_dato,
     cast(null as varchar(255)) as avsluttet_arsak,
     forste_dato_virk_fom as gyldig_fra_dato,
-    cast(null as date) as gyldig_til_dato
+    cast(null as date) as gyldig_til_dato,
+    cast(systimestamp at time zone 'UTC' as timestamp(9)) as kjoretidspunkt
 from ref_int_lopende_vedtak_ufore
